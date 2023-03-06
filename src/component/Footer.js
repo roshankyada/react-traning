c61412c9c98d50b42dcf992e5afba29a60471ac1
roshan kyada <roshanpatel00325@gmail.com>
2023-03-06 16:34:57 +0530

    Updating State Immutably 2

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,37 +1,21 @@
-
-import { useState } from 'react'
-
+import React, { useState } from 'react';
 
 export default function Footer() {
+    const [state, setState] = useState({
+        name: 'RK',
+        age: ''
+    });
 
-    const initState = ['Ford', 'BMW', 'Audi'];
-
-    const [persons, setpersons] = useState(initState)
-
-    const handelclick = () => {
-        // persons.push('Brain')
-        // persons.push('quber')
-        // setpersons(persons)
-
-        const newpersons = [...persons]
-        persons.push('Brain')
-        persons.push('quber')
-        setpersons(newpersons)
+    function handleNameChange(event) {
+        const newName = event.target.value;
+        setState(prevState => ({ ...prevState, name: newName }));
     }
 
-    console.log('hello')
     return (
-
         <div>
-
-            <button onClick={handelclick}>Click</button>
-            {
-                persons.map(person => (
-                    <div key={person}>{person}</div>
-                ))
-
-            }
+            <input type="text" value={state.name} onChange={handleNameChange} />
+            <p>Name: {state.name}</p>
+            <p>Age: {state.age}</p>
         </div>
-    )
-
+    );
 }
