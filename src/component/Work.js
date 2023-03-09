70cf02c786a3820ec88d083060cc2b96a60f3841
roshan kyada <roshanpatel00325@gmail.com>
2023-03-09 14:55:21 +0530

    Setting Class Names Dynamically

--- src/component/Work.js
+++ src/component/Work.js
@@ -1,29 +1,40 @@
-import React, { useState } from 'react'
+import React from 'react'
+import './my-style.module.css';
+// import Radium from 'radium';
 
 const Work = () => {
 
-    const [count, setCount] = useState(0);
-    const styles = {
-        background: 'green',
-        font: 'inherit',
-        border: '1px solid black',
-        padding: '8 px',
-        borderRadius: '7px',
-        boxShadow: '0 4px 5px #ccc',
-    };
+    // const [count, setCount] = useState(0);
+    // const styles = {
+    //     background: 'green',
+    //     font: 'inherit',
+    //     border: '1px solid black',
+    //     padding: '8 px',
+    //     borderRadius: '7px',
+    //     boxShadow: '0 4px 5px #ccc',
+    // };
+
+    const x = 60;
+    let color = [];
+    if (x <= 50) {
+        color.push('red');
+    } else if (x >= 51) {
+        color.push('green');
+    }
+
+
 
     return (
         <div>
 
-            <p>my name roshan, and my age {count}</p>
+            {/* <p>my name roshan, and my age {count}</p>
             <button onClick={() => setCount(count + 1)} style={styles} >
                 Click me
-            </button>
-
-
+            </button> */}
+            <h1 className={color} >{x}</h1>
         </div>
     )
 }
 
-export default Work
+export default Work;
 
