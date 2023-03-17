358339563dc97d2b110059fba13070bfd27566f8
roshan kyada <roshanpatel00325@gmail.com>
2023-03-17 16:57:58 +0530

    Refs with React Hooks

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,40 +1,21 @@
-import React, { useEffect, useState } from 'react'
+import React, { useRef } from 'react'
 
+const Footer = () => {
+    let inputref = useRef(null)
 
-const Footer = (props) => {
-
-
-
-    const [state, setState] = useState(null);
-
-
-
-    useEffect(() => {
-        console.log('[Footer.js] useEffect');
-
-        return () => {
-            console.log('[Footer.js] cleanup work in useEffect'); // cleanup work here
-        };
-    }, [])
-
-
-
+    function handelInput() {
+        console.log("roshan")
+        inputref.current.value = "100"
+        inputref.current.focus()
+        inputref.current.style.color = "red"
+        inputref.current.style.display = "none"
+    }
     return (
-
         <div>
-
-
-
-
-
-            <h2>rollNo:{props.rollNo}</h2>
-            <button
-                onClick={() => {
-                    setState('Some v. important state.');
-                }}> Click me</button>
-            <p>state: {state}</p>
-            <h1>{props.hocsub}</h1>
+            <input type="text" ref={inputref} />
+            <button onClick={handelInput}>ref</button>
         </div>
     )
 }
+
 export default Footer
